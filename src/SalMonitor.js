840fb72f242ffbe7360cbccf886df5bf2d455d57
Helder <he7d3r@gmail.com>
2019-01-01 20:07:37 -0200

    Check for API errors

--- src/SalMonitor.js
+++ src/SalMonitor.js
@@ -7,57 +7,61 @@
 ( function ( mw, $ ) {
 	'use strict';
 
-    function salMonitor() {
-      $.ajax( {
-            url: 'https://wikitech.wikimedia.org/w/api.php',
-            data: {
-                action: 'query',
-                formatversion: 2,
-                prop: 'revisions',
-                format: 'json',
-                rvprop: 'content',
-                rvsection: 2, // Current day only
-                titles: 'Server Admin Log'
-            },
-            dataType: 'jsonp'
-        } ).done( function ( data ) {
-            var text = data.query.pages[ 0 ].revisions[ 0 ].content,
-                regex = mw.config.get(
-        		'server-admin-log-regex',
-        		new RegExp( mw.RegExp.escape( mw.config.get( 'wgDBname') ) )
-		),
-                key;
-            $.each( text.split( '\n' ), function ( n, text ) {
-                if ( regex.test( text ) ) {
-                    key = '-s-a-log-' + text.substring( 0, 20 );
-                    if ( mw.cookie.get( key ) !== '1' ) {
-                        mw.notify(
-                        	$( '<a></a>' )
-                        		.attr( 'href', 'https://wikitech.wikimedia.org/wiki/Server_Admin_Log' )
-                        		.text( text ),
-                        	{
-	                        	autoHide: false,
-	                        	title: 'Server Admin Log',
-	                        	type: 'info'
-	                        }
-                        );
-                        mw.cookie.set(
-                            key,
-                            '1',
-                            {
-                                // 24 hours
-                                expires: 86400
-                            }
-                        );
-                    }
-                }
-            } );
-        } );
-    }
+	function salMonitor() {
+	  $.ajax( {
+			url: 'https://wikitech.wikimedia.org/w/api.php',
+			data: {
+				action: 'query',
+				formatversion: 2,
+				prop: 'revisions',
+				format: 'json',
+				rvprop: 'content',
+				rvsection: 2, // Current day only
+				titles: 'Server Admin Log'
+			},
+			dataType: 'jsonp'
+		} ).done( function ( data ) {
+			var text,
+				key,
+				regex = mw.config.get(
+					'server-admin-log-regex',
+					new RegExp( mw.RegExp.escape( mw.config.get( 'wgDBname') ) )
+				);
+			if ( !data || data.error ) {
+				return;
+			}
+			text = data.query.pages[ 0 ].revisions[ 0 ].content;
+			$.each( text.split( '\n' ), function ( n, text ) {
+				if ( regex.test( text ) ) {
+					key = '-s-a-log-' + text.substring( 0, 20 );
+					if ( mw.cookie.get( key ) !== '1' ) {
+						mw.notify(
+							$( '<a></a>' )
+								.attr( 'href', 'https://wikitech.wikimedia.org/wiki/Server_Admin_Log' )
+								.text( text ),
+							{
+								autoHide: false,
+								title: 'Server Admin Log',
+								type: 'info'
+							}
+						);
+						mw.cookie.set(
+							key,
+							'1',
+							{
+								// 24 hours
+								expires: 86400
+							}
+						);
+					}
+				}
+			} );
+		} );
+	}
 
-    if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Watchlist' ) {
-        mw.loader.using( [ 'mediawiki.cookie', 'mediawiki.notify', 'mediawiki.RegExp' ] )
-        .done( salMonitor );
-    }
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Watchlist' ) {
+		mw.loader.using( [ 'mediawiki.cookie', 'mediawiki.notify', 'mediawiki.RegExp' ] )
+		.done( salMonitor );
+	}
 
 }( mediaWiki, jQuery ) );
